6eee3c07461bcc0374bcc92c705922c71e11b454
Théo Larue <theo.larue@ryax.tech>
2021-01-30 23:11:58 +0100

    bindings

--- config.h
+++ config.h
@@ -5,26 +5,27 @@
 #define TERMCLASS "St"
 #define BROWSER "firefox"
 #define BROWSERCLASS "Firefox"
+#define FONT "Cousine Nerd Font Mono:style=Regular:size=12"
 
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const unsigned int systrayspacing = 3;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
 static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const int user_bh            = 0;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "Cousine Nerd Font Mono:style=Regular:size=12"  };
-static const char dmenufont[]       = "Cousine Nerd Font Mono:style=Regular:size=12";
+static const char *fonts[]          = { FONT };
+static const char dmenufont[]       = FONT;
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -66,12 +67,15 @@ static const int attachbelow = 1;    /* 1 means attach after the currently activ
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "TTT",      bstack },
-	{ "[@]",      spiral },
-	{ "[\\]",     dwindle },
-	{ "H[]",      deck },
-	{ "[M]",      monocle },
+	{ "[]=",      tile },    /* Default: Master on left, stack on the right */
+	{ "TTT",      bstack }, /* Master on top, stack on bottom */
+
+	{ "[@]",      spiral }, /* Fibonacci spiral */
+	{ "[\\]",     dwindle }, /* Decreasing in size right and leftward */
+
+	{ "H[]",      deck }, /* Master on left, monocle stack on the right */
+	{ "[M]",      monocle }, /* Everything on top of each other */
+
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 };
 
@@ -101,23 +105,22 @@ static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+
+	/* clients */
 	STACKKEYS(MODKEY,                          focus)
 	STACKKEYS(MODKEY|ShiftMask,                push)
-//	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-//	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_space,  zoom,            {0} },
-	{ MODKEY|Mod4Mask,              XK_u,      incrgaps,       {.i = +1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_u,      incrgaps,       {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_0,      togglegaps,     {0} },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
+	{ MODKEY,             					XK_f,      togglefullscr,  {0} },
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
+	{ MODKEY,                       XK_s,      togglesticky,   {0} },
+//	{ MODKEY,                       XK_o,      incnmaster,     {.i = +1 } },
+//	{ MODKEY|ShiftMask,             XK_O,      incnmaster,     {.i = -1 } },
+
+	/* layouts */
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} }, // tile
 	{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[1]} }, // bstacks
 	{ MODKEY,    			              XK_y,      setlayout,      {.v = &layouts[2]} }, // spiral
@@ -125,13 +128,28 @@ static Key keys[] = {
 	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[4]} }, // deck
 	{ MODKEY|ShiftMask,             XK_u,      setlayout,      {.v = &layouts[5]} }, // monocle
 	{ MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[6]} }, // floating
-	{ MODKEY,             					XK_f,      togglefullscr,  {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_s,      togglesticky,   {0} },
+
+	/* gaps */
+	{ MODKEY,              	        XK_g,      togglegaps,     {0} },
+	{ MODKEY|ShiftMask,             XK_g,      defaultgaps,    {0} },
+	{ MODKEY,                       XK_w,      incrgaps,       {.i = +1 } },
+	{ MODKEY, 									    XK_x,      incrgaps,       {.i = -1 } },
+
+	/* dmenu, scratchpad */
+	{ MODKEY,                       XK_c,      togglescratch,  {.v = scratchpadcmd } },
+	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
+
+	/* multi monitors */
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+
+	/* status bar */
+	{ MODKEY,                       XK_b,      togglebar,      {0} },
+
+	/* tags */
+	{ MODKEY,                       XK_Tab,    view,           {0} },
 	TAGKEYS(                        XK_ampersand,              0)
 	TAGKEYS(                        XK_eacute,			           1)
 	TAGKEYS(                        XK_quotedbl,			         2)
