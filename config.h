8d33786c3f40258df4f265e80bb84927f96bb0a2
Théo Larue <theo.larue@ryax.tech>
2021-01-30 18:03:23 +0100

    bar height

--- config.h
+++ config.h
@@ -10,6 +10,7 @@ static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int user_bh            = 0;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const char *fonts[]          = { "Cousine Nerd Font Mono:style=Regular:size=12"  };
 static const char dmenufont[]       = "Cousine Nerd Font Mono:style=Regular:size=12";
 static const char col_gray1[]       = "#222222";
