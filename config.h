7c4afc76ba5a4041fe77a5fe5115e1cc66f9225e
Théo Larue <theo.larue@ryax.tech>
2021-01-30 20:33:10 +0100

    sticky

--- config.h
+++ config.h
@@ -118,6 +118,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[6]} }, // floating
 	{ MODKEY,             					XK_f,      togglefullscr,  {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY,                       XK_s,      togglesticky,   {0} },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
