13defb92b02fb265c49d704d3cfa5df5d4d3b5d1
Théo Larue <theo.larue@ryax.tech>
2021-01-31 22:37:12 +0100

    centered

--- config.h
+++ config.h
@@ -46,15 +46,15 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class          			instance    title      tags mask    isfloating   isterminal  noswallow  monitor */
-	{ NULL,    					    NULL,         "Event Tester",   0,            0,           0,         1,        -1 },
-	{ TERMCLASS,   					NULL,         NULL,       	    0,            0,           1,         0,        -1 },
-	{ "Gnome-calculator",   NULL,         NULL,       	    0,            1,           1,         0,        -1 },
-	{ "Pavucontrol",   			NULL,         NULL,       	    0,            1,           1,         0,        -1 },
-	{ "Blueman-manager",    NULL,         NULL,       	    0,            1,           1,         0,        -1 },
-	{ "MEGAsync",   				NULL,         NULL,       	    0,            1,           1,         0,        -1 },
-	{ "Transmission-gtk",   NULL,         NULL,       	    0,            1,           1,         0,        -1 },
-	{ NULL, 							  "scratchpad", NULL, 			      0, 						1, 					 1, 				0, 				-1 },
+	/* class          			instance      title           tags mask  iscentered  isfloating  isterminal  noswallow  monitor */
+	{ NULL,    					    NULL,         "Event Tester", 0,         0,          0,          0,          1,        	-1 },
+	{ TERMCLASS,   					NULL,         NULL,       	  0,         0,          0,          1,          0,        	-1 },
+	{ "Gnome-calculator",   NULL,         NULL,       	  0,         1,          1,          0,          1,        	-1 },
+	{ "Pavucontrol",   			NULL,         NULL,       	  0,         1,          1,          0,          1,        	-1 },
+	{ "Blueman-manager",    NULL,         NULL,       	  0,         0,          1,          0,          1,        	-1 },
+	{ "MEGAsync",   				NULL,         NULL,       	  0,         0,          1,          0,          1,        	-1 },
+	{ "Transmission-gtk",   NULL,         NULL,       	  0,         1,          1,          0,          1,        	-1 },
+	{ NULL, 							  "scratchpad", NULL, 			    0,         1,				   1,          0,          0,        	-1 },
 };
 
 /* LAYOUTS */
