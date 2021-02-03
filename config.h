9bb19e2a24ef2649e90245d588701812158e0a1e
Théo Larue <theo.larue@ryax.tech>
2021-02-04 00:05:26 +0100

    fix borders and border color

--- config.h
+++ config.h
@@ -34,7 +34,7 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-static const char col_red[] 				= "#772200";
+static const char col_red[] 				= "#af1b1b";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
